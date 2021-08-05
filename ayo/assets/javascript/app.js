bd20d0db8882d01d3939a518cafeaae06fd5001f
Cristi <68418256+cristilmao@users.noreply.github.com>
2021-08-05 21:53:44 +0300

    Add files via upload

---
+++ ayo/assets/javascript/app.js
@@ -0,0 +1,44 @@
+var _app = function () {
+    this.id = 0;
+    this.videoElement = null;
+    this.audioElement = null;
+    this.musicVolume = 1;
+    this.musicFadeIn = 4000;
+    this.skippedIntro = false;
+    this.backgroundToggler = false;
+    this.shouldIgnoreVideo = false;
+    this.effects = ["bounce", "flash", "pulse", "rubberBand", "shake", "swing", "tada", "wobble", "jello"];
+    this.brandDescription = [ 
+        "flowqx", 
+        "js, java, html",  
+        "linux developer",
+        "js developer",
+        "discord developer",
+        ";)"
+    ];
+
+    this.iconChanger = function (urls, delay) {
+        if (!urls)
+            return;
+
+        delay = delay || 1000;
+
+        var counter = 0;
+        
+        setInterval(function () {
+            if(counter < urls.length) {
+                var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+                link.type = 'image/x-icon';
+                link.rel = 'shortcut icon';
+                link.href = urls[counter];
+                document.getElementsByTagName('head')[0].appendChild(link);
+            }
+            else 
+                counter = 0;
+
+           ++counter;
+        }, delay);
+    }
+};
+
+var app = new _app();
